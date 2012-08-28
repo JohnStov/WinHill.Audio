a076ce9d6bc079ec31bc66bdb27c691a6cf3672e
John Stovin <john.d.stovin@gmail.com>
2012-08-28 14:44:19 +0100

    Initial test project

--- WinHill.Audio.Test/Properties/AssemblyInfo.cs
+++ WinHill.Audio.Test/Properties/AssemblyInfo.cs
@@ -1,18 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinHill.Audio")]
-[assembly: AssemblyDescription("A streaming audio library for .NET")]
+[assembly: AssemblyTitle("WinHill.Audio.Test")]
+[assembly: AssemblyDescription("Test for WinHill.Audio")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Win Hill Systems")]
-[assembly: AssemblyProduct("WinHill.Audio")]
+[assembly: AssemblyProduct("WinHill.Audio.Test")]
 [assembly: AssemblyCopyright("Copyright © John Stovin 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2a6f7e66-f60c-4f87-9bf6-b1536dbcac10")]
 
 // Version information for an assembly consists of the following four values:
 //
