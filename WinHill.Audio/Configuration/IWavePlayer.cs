cfe481f6594c442837e5f11a42c747fd36c40f69
John Stovin <john.d.stovin@gmail.com>
2012-09-05 15:26:56 +0100

    Htdrated IWavePlayer

--- WinHill.Audio/Configuration/IWavePlayer.cs
+++ WinHill.Audio/Configuration/IWavePlayer.cs
@@ -4,5 +4,12 @@ namespace WinHill.Audio.Configuration
 
     public interface IWavePlayer : IDisposable
     {
+        PlaybackState PlaybackState { get; }
+
+        void Play();
+
+        void Stop();
+
+        void Pause();
     }
 }
---
+++ WinHill.Audio/Configuration/PlaybackState.cs
@@ -0,0 +1,10 @@
+namespace WinHill.Audio.Configuration
+{
+    public enum PlaybackState
+    {
+        Unknown,
+        Stopped,
+        Paused,
+        Playing
+    }
+}
